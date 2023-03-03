a04bb6ff4bdb8a39eba88b688e9aed2b7550237d
therealbeez <gabriel.agbese2@gmail.com>
2023-03-03 18:57:30 +0100

    calendar

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { ReactEmbeddedGoogleCalendar } from 'react-embedded-google-calendar';
+
+
 
 export default function Events() {
   return (
     <div className='events-wrapper'>
         <div className='events-side'>
           <p className='events-name'> Events</p>
+          <p className='upcoming-title'> Upcoming Event:</p>
+          <p className='upcoming-event'> Gallery Exhibition</p>
+          <p className='upcoming-date'> 1st January 2001</p>
+          <div className='calendar'>
+            <ReactEmbeddedGoogleCalendar publicUrl ="https://calendar.google.com/calendar/embed?src=cavicnigeriainfo%40gmail.com&ctz=UTC"/>
+          </div>
         </div>
         <div className='events-list'>
             <div className='event-item'>1</div>
