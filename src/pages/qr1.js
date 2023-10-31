92bcb1e29f9b16ce44e90f50cae5fc4267b7d1c1
therealbeez <gabriel.agbese2@gmail.com>
2023-10-31 16:08:44 +0100

    autoplay vid

--- src/pages/qr1.js
+++ src/pages/qr1.js
@@ -7,19 +7,18 @@ import "../components/layout.css"
 function qr1() {
     return (
         <div className='art-details'>
-            <p className='art-name'>Art Name</p>
-            <p className='artist-name '>By Philip Agbese </p>
-            <video height="400" width="400" controls className='vid-container'>
+            {/* <p className='art-name'>Art Name</p>
+            <p className='artist-name '>By Philip Agbese </p> */}
+            <video height="400" width="400" controls autoPlay muted className='vid-container'>
                 <source src={qrvid1} type="video/mp4" />
             </video>
-            <div className='ar-container'>
+            {/* <div className='ar-container'>
                 <p>View art live in Augument Reality with Artevive:</p>
                 <div className='ar-sub-container'>
                     <FontAwesomeIcon icon={faGooglePlay} />
                     <FontAwesomeIcon icon={faAppStore} />
                 </div>
-            </div>
-
+            </div> */}
         </div>
     )
 }
