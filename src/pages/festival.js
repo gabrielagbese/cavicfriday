2ccde1382170546296b57cf3b2a944c23767cdcf
Gabriel Agbese <gabriel.agbese2@gmail.com>
2024-05-06 18:34:44 +0100

    festival ecorevived redirect

---
+++ src/pages/festival.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+function festival() {
+
+    // useEffect(() => {
+    //     window.location.replace('https://www.google.com')
+    // }, [])
+    window.location.replace('https://ecorevived.com/')
+
+    return (
+        <div></div>
+    )
+}
+
+export default festival
