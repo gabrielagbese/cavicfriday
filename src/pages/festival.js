689bed8e2fa70f59bf2ffa80000ac62041c2367a
Gabriel Agbese <gabriel.agbese2@gmail.com>
2024-05-26 16:35:17 +0100

    window ecorevived fest

--- src/pages/festival.js
+++ src/pages/festival.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 function Festival() {
 
     useEffect(() => {
-        window.location.replace('https://www.ecorevived.com')
+        <iframe src='https://www.ecorevived.com' className='ifr'>a</iframe>
     }, [])
 
 
