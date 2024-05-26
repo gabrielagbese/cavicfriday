8810d49a5d90bcc917e19d4946be852824b9191e
Gabriel Agbese <gabriel.agbese2@gmail.com>
2024-05-26 17:06:02 +0100

    forced inline styling

--- src/pages/festival.js
+++ src/pages/festival.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import { useEffect } from 'react'
 
+const style = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    width: "100%",
+    height: "100%",
+    border: "none",
+    margin: 0,
+    padding: 0,
+    overflow: "hidden",
+    zIndex: 999999
+};
 function Festival() {
+
 
     useEffect(() => {
 
@@ -9,7 +24,7 @@ function Festival() {
 
 
     return (
-        <iframe src='https://ecorevived.com/' className='ifr'>a</iframe>
+        <iframe src='https://ecorevived.com/' className='ifr' style={style}>a</iframe>
     )
 }
 
