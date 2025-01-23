1113ccaf0dbe4194ca43accfe6835cd8da0ea0ff
Gabriel Agbese <gabriel.agbese2@gmail.com>
2025-01-23 15:12:14 +0000

    11.jpg cover

--- src/components/spaces-content.js
+++ src/components/spaces-content.js
@@ -4,7 +4,7 @@ const SpacesContent = {
     pics3: require('../images/reels/spaces/3.jpg').default,
     pics4: require('../images/reels/spaces/4.jpg').default,
     pics5: require('../images/reels/spaces/5.jpg').default,
-    pics6: require('../images/reels/spaces/6.jpg').default,
+    pics6: require('../images/reels/spaces/11.jpg').default,
     pics7: require('../images/reels/spaces/7.jpg').default,
     pics8: require('../images/reels/spaces/8.jpg').default,
     pics9: require('../images/reels/spaces/9.jpg').default,
